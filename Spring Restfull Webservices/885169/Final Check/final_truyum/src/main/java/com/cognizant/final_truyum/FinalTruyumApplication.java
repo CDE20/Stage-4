fdb594480b99d01db165c2bc008914b73cd58b04
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-03-06 12:41:27 +0530

    Add files via upload

---
+++ Spring Restfull Webservices/885169/Final Check/final_truyum/src/main/java/com/cognizant/final_truyum/FinalTruyumApplication.java
@@ -0,0 +1,19 @@
+package com.cognizant.final_truyum;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class FinalTruyumApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FinalTruyumApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTruyumApplication.class, args);
+		LOGGER.info("inside main");
+	}
+
+}
---
+++ Spring Restfull Webservices/885169/Final Check/final_truyum/src/main/java/com/cognizant/final_truyum/service/MovieService.java
@@ -0,0 +1,37 @@
+package com.cognizant.final_truyum.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.final_truyum.dao.MovieDao;
+import com.cognizant.final_truyum.model.Movie;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	MovieDao dao;
+	
+	public List<Movie> getMovieListAdmin()
+	{
+		return dao.getMovieListAdmin();
+	}
+	
+	public List<Movie> getMovieListCustomer()
+	{
+		return dao.getMovieListCustomer();
+	}
+	
+	public Movie getMovie(long movieId)
+	{
+		return dao.getMovie(movieId);
+	}
+	
+	public void modifyMovie(Movie movie)
+	{
+		dao.modifyMovie(movie);
+	}
+
+}
