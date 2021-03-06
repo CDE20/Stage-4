ca6711b2c4bc392f577c52ed1f4a5962ac796838
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-03-06 16:09:54 +0530

    Add files via upload

---
+++ Spring Restfull Webservices/885149/Practice Check/truyum/src/main/java/com/cognizant/truyum/TruyumApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.truyum;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.truyum.dao.MenuItemDaoCollectionImpl;
+import com.cognizant.truyum.model.MenuItem;
+import com.cognizant.truyum.service.MenuItemDaoSerivce;
+
+
+@SpringBootApplication
+public class TruyumApplication 
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(TruyumApplication.class);
+	private static MenuItemDaoSerivce menuimpl;
+    public static void main( String[] args )
+    {
+    	LOGGER.info("Inside Main");
+    	ApplicationContext context = SpringApplication.run(TruyumApplication.class,args);
+    	menuimpl=context.getBean(MenuItemDaoSerivce.class);
+    	//test();
+    }
+    
+    private static void test() {
+    	LOGGER.info("Start");
+    	List<MenuItem> menu = menuimpl.getAllMenuItems();
+    	LOGGER.debug("MenuItem={}",menu);
+    	LOGGER.info("Stop");
+    }
+}
