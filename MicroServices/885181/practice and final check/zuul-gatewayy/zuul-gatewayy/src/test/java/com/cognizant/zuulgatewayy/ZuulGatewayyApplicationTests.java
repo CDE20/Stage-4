db59177c474df2213ec6d28a2994c4145ab5c9f0
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-03-13 01:16:01 -0800

    Add files via upload

---
+++ MicroServices/885181/practice and final check/zuul-gatewayy/zuul-gatewayy/src/test/java/com/cognizant/zuulgatewayy/ZuulGatewayyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.zuulgatewayy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
