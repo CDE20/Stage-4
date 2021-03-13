1a009c15ee1979b2f6d66ad84ee897eebfbddd71
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-03-13 11:12:57 +0530

    Add files via upload

---
+++ MicroServices/885124/Day 1 to 3/Practice and Final Check/movie-service/final_truyum/src/main/java/com/cognizant/final_truyum/FinalTruyumApplication.java
@@ -0,0 +1,21 @@
+package com.cognizant.final_truyum;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FinalTruyumApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FinalTruyumApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTruyumApplication.class, args);
+		LOGGER.info("inside main");
+	}
+
+}
---
+++ MicroServices/885124/Day 1 to 3/Practice and Final Check/movie-service/final_truyum/src/main/java/com/cognizant/final_truyum/exception/FavoritesEmptyException.java
@@ -0,0 +1,9 @@
+package com.cognizant.final_truyum.exception;
+
+public class FavoritesEmptyException extends Exception {
+	public FavoritesEmptyException(String message)
+	{
+		super(message);
+	}
+
+}
