b61dce69c81a6893767e60c2891c963dfd508896
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-03-13 14:20:31 +0530

    Add files via upload

---
+++ MicroServices/885181/practice and final check/eureka-discovery-serverr/eureka-discovery-serverr/src/test/java/com/cognizant/eurekadiscoveryserverr/EurekaDiscoveryServerrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.eurekadiscoveryserverr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
